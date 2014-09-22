e5a8c57a36799ef9297f66affde4b91e2b5fce53
SHIE, Li-Yi <lyshie@mx.nthu.edu.tw>
2014-09-22 23:45:02 +0800

    Add simple tests to demonstrate setjmp() and longjmp()

---
+++ jmp/jmp-nested.c
@@ -0,0 +1,57 @@
+// =====================================================================================
+//
+//       Filename:  jmp-nested.c
+//
+//    Description:  setjmp() / longjmp()
+//
+//        Version:  1.0
+//        Created:  2014-09-22 22:09:18
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  SHIE, Li-Yi (lyshie), lyshie@mx.nthu.edu.tw
+//        Company:
+//
+// =====================================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <setjmp.h>
+
+jmp_buf task1;
+jmp_buf task2;
+
+void func2(int n)
+{
+    if (!setjmp(task2))
+    {
+        printf("task2, setjmp() => %d, %x\n", n, &n);
+        longjmp(task2, 1);
+    }
+    else
+    {
+        printf("task2, back => %d, %x\n", n, &n);
+    }
+}
+
+void func1(int n)
+{
+    if (!setjmp(task1))
+    {
+        printf("task1, setjmp() => %d, %x\n", n, &n);
+        func2(n);
+        longjmp(task1, 1);
+    }
+    else
+    {
+        printf("task1, back => %d, %x\n", n, &n);
+    }
+
+}
+
+int main(int argc, char **argv)
+{
+    func1(99);
+
+    return EXIT_SUCCESS;
+}
